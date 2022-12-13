8e9d887c9f1d8b60f0a78c31cabf65f80dea9eb8
smw <1135192606@qq.com>
2022-12-13 16:40:09 +0800

    feat: add prisma && cors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient, test_tabel } from '@prisma/client'
+import {cors, runMiddleware} from "./cors";
 
 type Data = {
-  name: string
+  name: string,
+  num: test_tabel[]
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  await runMiddleware(req, res, cors)
+
+  const prisma = new PrismaClient()
+  const num = await prisma.test_tabel.findMany()
+  res.status(200).json({ name: 'John Doe 22', num: num })
 }
